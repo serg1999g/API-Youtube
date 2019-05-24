d7d97ad9e732f2030e46f4ad71f3c969c6761bd9
serg1999g <46674079+serg1999g@users.noreply.github.com>
2019-05-24 20:01:58 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+//    This code loads the IFrame Player API code asynchronously.
+let tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+let firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+//    This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+let player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '360',
+        width: '640'
+    });
+}
+
+let btn = document.querySelector('button'),
+    url = document.querySelector('#url');
+
+btn.addEventListener("click", function () {
+    let id = url.value.split('='),
+        playlist_id = id[1].split('&')[0];
+    // console.log(id);
+    player.loadVideoByUrl("http://www.youtube.com/v/" + playlist_id);
+
+});
